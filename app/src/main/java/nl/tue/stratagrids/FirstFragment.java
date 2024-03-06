51fe520f6264bf07e1d5d9b481984ee0bcce6200
Thijs van Berkel <m.p.j.v.berkel@student.tue.nl>
2024-03-06 19:50:06 +0100

    Created the basic overview UI, no styling yet

--- app/src/main/java/nl/tue/stratagrids/FirstFragment.java
+++ app/src/main/java/nl/tue/stratagrids/FirstFragment.java
@@ -29,13 +29,15 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        //Generated code by android studio, but the button was removed. Might be useful for later
+        //~Thijs
+        /*binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
-        });
+        });*/
     }
 
     @Override
